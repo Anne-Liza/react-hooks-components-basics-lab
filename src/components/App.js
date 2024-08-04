8336ab0f85112f80f24fb8266b948d4028deb8ad
Anne <annewangui210@gmail.com>
2024-08-04 17:48:47 +0300

    react-hooks-components-basics-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import image from '../images/demo.png';
+
+const user ={
+  name: "Anne Liza",
+  location: "New york",
+  bio: "a Web Developer",
+  github: "https://github.com/Anne-Liza",
+  linkedin: "https://www.linkedin.com/in/annelizawangui/"
+
+};
 
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
     </nav>
   );
 }
@@ -11,17 +22,28 @@ function NavBar() {
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h1>{user.name} is {user.bio} from {user.location}.</h1>
     </div>
   );
 }
 
 function About(){
-  return(
-    <div id="about">
-      <h2>About</h2>
-    </div>
-  );
+
+  if(!user.bio){
+    return null;
+  }else {
+    return(
+      <div id="about">
+        <h2>About</h2>
+        <p>I made this!</p>
+        <img src={image} alt="Imade this"/>
+        <p><a href={user.github}>{user.github}</a></p>
+        <p><a href={user.linkedin}>{user.linkedin}</a></p>
+       
+      </div>
+    );
+  }
+ 
 }
 
 
